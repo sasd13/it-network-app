3333d0e2d0d5369ad81f9a4032d44eed187b8e1d
lioubouchkin.pavel <rabo4i79@orange.fr>
2017-01-26 23:50:13 +0100

    login - 1 level register - 1 level

--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild  } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserLogin } from 'models';
+import { NgForm } from '@angular/forms';
 
 import { AuthenticationService } from '../../services/index';
 
@@ -10,12 +11,66 @@ import { AuthenticationService } from '../../services/index';
 })
 export class LoginComponent  {
     model = new UserLogin();
+    @ViewChild(NgForm)
+    ngForm: NgForm;
     constructor(
             private authService: AuthenticationService,
             private router : Router
     ) { }
 
     login() {
-        return this.authService.authenticate(this.model);
+        if (this.ngForm.form.invalid) {
+            return;
+        }
+        this.authService.authenticate(this.model).
+            then(success => {
+                this.router.navigate(['/']);
+            },
+                error =>{
+                // TODO treat identification error
+            });
     }
+
+    formErrors = {
+        'username': '',
+        'password': ''
+    };
+
+     ngAfterViewChecked() {
+        this.formChanged();
+    }
+
+    formChanged() {
+        if (this.ngForm) {
+        this.ngForm.valueChanges
+            .subscribe(data => this.onValueChanged(data));
+        }
+    }
+
+    onValueChanged(data?: any) {
+        if (!this.ngForm) { return; }
+        const form = this.ngForm.form;
+
+        for (const field in this.formErrors) {
+        // clear previous error message (if any)
+        this.formErrors[field] = '';
+        const control = form.get(field);
+
+            if (control && control.dirty && !control.valid) {
+                const messages = this.validationMessages[field];
+                for (const key in control.errors) {
+                this.formErrors[field] += messages[key] + ' ';
+                }
+            }
+        }
+    }
+
+    validationMessages = {
+        'username': {
+            'required': 'Username is required.'
+        },
+        'password': {
+            'required': 'Password is required.'
+        }
+    };
 }
--- src/app/components/register/register.ts
+++ src/app/components/register/register.ts
@@ -23,5 +23,55 @@ export class RegisterComponent {
         if (this.ngForm.form.invalid) {
             return;
         }
+
+         // TODO promise returned:
+         this.registrationService.register(this.model)
+             .then( data => {
+                //  this.loading = true;
+                 this.router.navigate(['/login']);
+        });
     }
+
+    formErrors = {
+        'username': '',
+        'password': ''
+    };
+
+     ngAfterViewChecked() {
+        this.formChanged();
+    }
+
+    formChanged() {
+        if (this.ngForm) {
+        this.ngForm.valueChanges
+            .subscribe(data => this.onValueChanged(data));
+        }
+    }
+
+    onValueChanged(data?: any) {
+        if (!this.ngForm) { return; }
+        const form = this.ngForm.form;
+
+        for (const field in this.formErrors) {
+        // clear previous error message (if any)
+        this.formErrors[field] = '';
+        const control = form.get(field);
+
+            if (control && control.dirty && !control.valid) {
+                const messages = this.validationMessages[field];
+                for (const key in control.errors) {
+                this.formErrors[field] += messages[key] + ' ';
+                }
+            }
+        }
+    }
+
+    validationMessages = {
+        'username': {
+            'required': 'Username is required.'
+        },
+        'password': {
+            'required': 'Password is required.'
+        }
+    };
 }
